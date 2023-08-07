8ad467a86a23e249ae3e8d12630695cb69bc7572
agSuperCoder123 <alex@ahoy.ooo>
2023-08-07 10:44:56 -0400

    init wallet test

--- src/App.svelte
+++ src/App.svelte
@@ -1,13 +1,62 @@
 <script>
-	export let name;
 	import Slideshow from './Slideshow.svelte';
+	import { wallet } from './WalletStore.js';
+	import { onMount } from 'svelte';
+	import { ethers } from 'ethers';
+	import { Wallet } from '@wagmi/core';
+	
+	let provider;
+	let signer;
+	let connected = false;
+
+	onMount(() => {
+	  setupWalletConnect();
+	});
+	
+	async function setupWalletConnect() {
+	  provider = new Wallet.Provider.WalletConnectProvider({
+		rpc: {
+		  1: "https://mainnet.infura.io/v3/YOUR_INFURA_PROJECT_ID"
+		  // Add other networks if necessary
+		}
+	  });
+	  
+	  await provider.enable();
+	  
+	  signer = new ethers.providers.Web3Provider(provider).getSigner();
+	  wallet.set(signer);  // Update the wallet store with the connected signer
+	  connected = true;
+	}
+	
+	async function disconnect() {
+	  await provider.close();
+	  wallet.set(null);
+	  connected = false;
+	}
 </script>
 
 <main>
-	<h1>babbycakes Slideshow App</h1>
+	<h1>babbycakes 11 Slideshow App</h1>
 	<Slideshow />
+	<header>
+		This is a header (html)
+	</header>
+	<div>
+		<h3>Updates</h3>
+		<ul><li>Added WalletStore </li></ul>
+	</div>
+
+	<!-- WalletConnect UI elements -->
+	<div>
+		{#if connected}
+			<button on:click={disconnect}>Disconnect</button>
+		{:else}
+			<button on:click={setupWalletConnect}>Connect with WalletConnect</button>
+		{/if}
+	</div>
+
 	<footer>
-		Is this really html footer?
+		Is this reayoooolly html footer? (svelte updated)
 	</footer>
 	<b>how can svelte be used to link up to other things?</b>
 </main>
@@ -21,7 +70,7 @@
 	}
 
 	h1 {
-		color: #ff3e00;
+		color: #0040ff;
 		text-transform: uppercase;
 		font-size: 4em;
 		font-weight: 100;
---
+++ src/WalletStore.js
@@ -0,0 +1,4 @@
+// WalletStore.js
+import { writable } from 'svelte/store';
+
+export const wallet = writable(null);
